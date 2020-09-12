d28d9265edca775c3a0c5c6d2736b2ff2b16d5a9
amyrabdul <amyrabdul@hotmail.com>
2020-09-11 22:51:20 -0300

    exercicio contagem

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace contagem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Para iniciar a contagem regressiva para destruição mundial aperte uma tecla!");
+            Console.ReadKey();
+            Console.WriteLine("Voce realmente tem certeza disso? Se sim pressione uma tecla");
+            Console.ReadKey();
+            Console.WriteLine("Mas porque voce quer destruir tudo? Se quiser continuar selecione uma tecla");
+            Console.ReadKey();
+            Console.WriteLine("Iniciando contagem!");
+            Console.ReadKey();
+            Console.WriteLine("10!");
+            Console.ReadKey();
+            Console.WriteLine("9!");
+            Console.ReadKey();
+            Console.WriteLine("8!");
+            Console.ReadKey();
+            Console.WriteLine("7!");
+            Console.ReadKey();
+            Console.WriteLine("6!");
+            Console.ReadKey();
+            Console.WriteLine("5!");
+            Console.ReadKey();
+            Console.WriteLine("4!");
+            Console.ReadKey();
+            Console.WriteLine("3!");
+            Console.ReadKey();
+            Console.WriteLine("2!");
+            Console.ReadKey();
+            Console.WriteLine("1!");
+            Console.ReadKey();
+            Console.ForegroundColor= ConsoleColor.Red;
+            Console.WriteLine("BOOOOMMMMM!");
+            Console.ReadKey();
+            Console.ResetColor ();
+            Console.WriteLine("BRINCADEIRINHAAA");
+            Console.ReadKey();
+            
+
+        }
+    }
+}
